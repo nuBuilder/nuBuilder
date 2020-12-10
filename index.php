fe245919eb9ff1a11bdeca29dc412da93d543496
smalos <45562597+smalos@users.noreply.github.com>
2020-12-10 11:02:33 +0100

    [Fixed] Index.php
    Blank page was shown, since $p undefined.
    Removed Wordpress code

--- index.php
+++ index.php
@@ -204,24 +204,13 @@ window.nuHASH				= [];
 	if ( $nuUser != ''  &&  $nuPassword != '' ){
 		$h2 = nuUseUP($nuBrowseFunction, $target, $welcome, $nuUser, $nuPassword);
 	}else{
-		
-		if($p){
-			
-			if($s){
-				$h2 = nuGetJS_action_screen($nuBrowseFunction, $target, $welcome, $opener, $search, $like);
-			}else{
-				$h2 = nuGetJS_plugin_login($nuBrowseFunction, $target);
-			}
-			
-		}else{
 			
 			if($opener == ''){
 				$h2 = nuGetJS_standalone_login($nuBrowseFunction, $target, $welcome);
 			}else{
 				$h2 = nuGetJS_action_screen($nuBrowseFunction, $target, $welcome, $opener, $search, $like);
 			}
-			
-		}
+
 	}
 
 	// end choose h2
