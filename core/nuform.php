nuBuilder/nuBuilder

commit 653ae573add7988310712dfbee02bedf12c8f8e3

[Fixed] nulog issue with PHP 8
DB Version: V.4.5-2021.01.01.00
Files Version: V.4.5-2021.01.01.01

core/nuform.php
@@ -56,8 +56,13 @@ function nuBeforeEdit($FID, $RID){
 				$S				= "SELECT $logfield FROM `$r->sfo_table` WHERE `$r->sfo_primary_key` = ? ";
 				
 				$T				= nuRunQuery($S, [$RID]);
-				$J				= db_fetch_row($T)[0];
-				$jd				= json_decode($J);
+
+				$jd = '';
+				if (db_num_rows($T) == 1) {
+					$J			= db_fetch_row($T)[0];
+					$L = $J[0];
+					$jd	= json_decode($J);
+				}
 				
 				if(gettype($jd) == 'object'){
 					$jd->viewed	= Array('user' => $user, 'time' => time());								

core/nucommon.js
@@ -630,7 +630,7 @@ function nuBindCtrlEvents(){
 				} else if(e.keyCode == 69 && window.global_access) {					//-- t		Database
 					nuStartDatabaseAdmin();		
 				} else if(e.keyCode == 85 && window.global_access) {					//-- u		Setup
-					nuForm('nusetup','1','', '');						
+					nuForm('nusetup','1','', '', 2);						
 				} else if(e.keyCode == 83) {											//-- s		Search
 					nuSearchAction();
 				} else if(e.keyCode == 65 && window.global_access) {					//-- a		Add
@@ -659,7 +659,7 @@ function nuBindCtrlEvents(){
 				} else if(e.keyCode == 68 && window.global_access) {					//-- d		nuDebug Results
 					nuPopup("nudebug", "");
 				} else if(e.keyCode == 85 && window.global_access) {					//-- u		Setup
-					nuForm('nusetup','1','', '');										
+					nuForm('nusetup','1','', '', 2);										
 				} else if(e.keyCode == 73 && window.global_access) {					//-- i		Form Info
 					nuShowFormInfo();					
 				} else if(e.keyCode == 69 && window.global_access) {					//-- t		Database

core/nuform.js
@@ -2121,7 +2121,7 @@ function nuGetOptionsList(f, t, p, a, type){
 		
 		list.push([nuTranslate('nuDebug Results'), 'nuPopup("nudebug", "")', 'fa-bug', 'Ctrl+Shft+D']);					
 		list.push([nuTranslate('Database'), 'nuStartDatabaseAdmin();', 'fa-database', 'Ctrl+Shft+E']);		
-		list.push([nuTranslate('Setup'), 'nuForm("nusetup","1","", "")', 'fa-cogs', 'Ctrl+Shft+U']);			
+		list.push([nuTranslate('Setup'), 'nuForm("nusetup","1","", "", 2)', 'fa-cogs', 'Ctrl+Shft+U']);			
 		list.push([nuTranslate('Form Info'), 'nuShowFormInfo();', 'fa-info', 'Ctrl+Shft+I']);					
 		
 	}	

commit d6629a89af1e00dd8c468f803810f5d02c4e5571

[Fixed] nuLog
Fix of regression caused by
https://github.com/smalos/nubuilder4-Dev/commit/653ae573add7988310712dfbee02bedf12c8f8e3

DB Version: V.4.5-2021.01.01.00
Files Version: V.4.5-2021.01.02.00

core/nuform.php
@@ -59,8 +59,7 @@ function nuBeforeEdit($FID, $RID){
 
 				$jd = '';
 				if (db_num_rows($T) == 1) {
-					$J			= db_fetch_row($T)[0];
-					$L = $J[0];
+					$J			= db_fetch_row($T)[0];					
 					$jd	= json_decode($J);
 				}
 				