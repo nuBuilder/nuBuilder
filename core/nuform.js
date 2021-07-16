01713da7e218aa70c98c8e649e7757cdf8910982
kev1n <45562597+smalos@users.noreply.github.com>
2021-07-16 10:57:24 +0200

    [Fixed] Cloning indicator position
    DB Version: V.4.5-2021.07.16.00
    Files Version: V.4.5-2021.07.16.05

--- core/nuform.js
+++ core/nuform.js
@@ -2507,7 +2507,7 @@ function nuGetOptionsList(f, t, p, a, type){
 
 			list.push([nuTranslate('nuDebug Results'), 'nuOptionsListAction("nudebug", "")', 'fa-bug', 'Ctrl+Shft+D']);
 			list.push([nuTranslate('Database'), 'nuStartDatabaseAdmin();', 'fa-database', 'Ctrl+Shft+E']);
-			list.push([nuTranslate('Backup'), 'nuRunPHPHidden("nubackup",0)', 'fa fa-hdd-o', 'Ctrl+Shft+E']);
+			list.push([nuTranslate('Backup'), 'nuRunPHPHidden("nubackup",0)', 'fa fa-hdd-o', 'Ctrl+Shft+B']);
 			list.push([nuTranslate('Setup'), 'nuForm("nusetup","1","", "", 2)', 'fa-cogs', 'Ctrl+Shft+U']);
 			list.push([nuTranslate('Form Info'), 'nuShowFormInfo();', 'fa-info', 'Ctrl+Shft+I']);
 
@@ -3647,7 +3647,9 @@ function nuCloneAction(){
 	nuEmptyNoClone();
 
 	var b	= $('.nuBreadcrumb').length;
-	var bc = b == 0 ? $('#nuHomeGap') : $('#nuBreadcrumb' + b);
+	var nb	= $('.nuNotBreadcrumb').not('#nuLogout').length;
+
+	var bc = b == 0 && nb == 0 ? $('#nuHomeGap') : $('#nuBreadcrumb' + b);
 	bc.append('&nbsp;<span class="nuCloning">&nbsp;'+ nuTranslate('Cloning') + '&nbsp;</span>');
 
 	if(window.nuOnClone){
