c5a57ae7dd002fada94c4a775a98943e2b721a36
kev1n <45562597+smalos@users.noreply.github.com>
2021-12-21 07:54:34 +0100

    [Improved] JS nuSelectRemoveOption()
    Pass a jQuery object or an object Id
    
    DB Version: V.4.5-2021.12.19.00
    Files Version: V.4.5-2021.12.21.03

--- core/nucommon.js
+++ core/nucommon.js
@@ -2594,12 +2594,18 @@ function nuSelectRemoveEmpty(i, setIndex) {
 
 function nuSelectRemoveOption(i, value) {
 
-	let o = $('#' + i);
+	var o;
+	if (typeof i !== 'object') {
+		o = $('#' + i) || i;
+	} else {
+		o = i;
+	}
+
 	o.find('[value="'+value+'"]').remove();
 	return o;
 
 }
-
+ 
 function nuSelectRemoveMultiple(i) {
 
 	var id = i === undefined || i == null ? 'select' : '#' + i;
