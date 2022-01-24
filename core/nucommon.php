c0be8c0c82576de84c4226291316c80d920ac136
kev1n <45562597+smalos@users.noreply.github.com>
2022-01-24 06:12:10 +0100

    [Added] PHP nuIsMobile()
    DB Version: V.4.5-2022.01.20.01
    Files Version: V.4.5-2022.01.24.02

--- core/nucommon.php
+++ core/nucommon.php
@@ -2089,4 +2089,8 @@ function nuDemo($showError = true) {
 
 }
 
+function nuIsMobile() {
+	return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+
 ?>
