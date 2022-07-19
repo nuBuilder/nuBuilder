45ca1da980006850337c01ef2ea5cdb6eb14d56b
kev1n <45562597+smalos@users.noreply.github.com>
2022-07-19 03:21:39 +0200

    [Added] Browse scroll behaviour for smaller screens
    DB Version: V.4.5-2022.07.19.00
    Files Version: V.4.5-2022.07.19.10

--- core/nuform.js
+++ core/nuform.js
@@ -327,11 +327,41 @@ function nuBuildForm(f) {
 	}
 
 	nuWindowPosition();
+	
+	nuBrowseScrollbar();
 
 	nuRestoreScrollPositions();
 
 }
 
+function nuBrowseScrollbar() {
+
+	if (nuFormType() == 'browse') {
+
+		if (!nuIsIframe()) {
+			$('#nuRECORD').css({
+				'width': '100vw',
+				'height': '100%',
+				'overflow-x': 'auto'
+			});
+			$('#nuBreadcrumbHolder').css({
+				'width': '100vw',
+				'display': 'flex',
+				'flex': '1',
+				'flex-flow': 'row wrap'
+			});
+			$('#nuActionHolder').css({
+				'width': '100vw'
+			});
+			document.body.style.overflow = 'hidden';
+		}
+
+	} else {
+		document.body.style.overflow = 'visible';
+	}
+
+}
+	
 function nuRestoreScrollPositions() {
 
 	$(function () {
