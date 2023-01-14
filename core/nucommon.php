f07d54f159edc18b426cae753562a986a819159c
kev1n <45562597+smalos@users.noreply.github.com>
2023-01-14 07:31:27 +0100

    [Fixed] PHP nuReplaceHashVariables() - (2)
    DB Version: V.4.5-2023.01.04.00
    Files Version: V.4.5-2023.01.14.00

--- core/nucommon.php
+++ core/nucommon.php
@@ -650,7 +650,7 @@ function nuReplaceHashVariables($s){
 	foreach ($a as $k => $v) {
 
 		 if(!is_object ($a[$k]) && !is_array ($a[$k]) ) {
-			$v === null ? '' : $v; 
+			$v = $v == null ? '' : $v; 
 			$s	= str_replace ('#' . $k . '#', $v, $s);
 		}
 
