dc77a7209a24ae2ef9de4fab5ee2ab78ab7b0568
kev1n <45562597+smalos@users.noreply.github.com>
2023-03-08 09:16:25 +0100

    [Removed] Duplicate JS nuPrintEditForm()
    DB Version: V.4.5-2023.03.08.00
    Files Version: V.4.5-2023.03.08.11

--- core/nuform.js
+++ core/nuform.js
@@ -4539,28 +4539,6 @@ function nuBrowseAdditionalNavButtons() {
 
 }
 
-function nuPrintEditForm() {
-
-	$('#nuBreadcrumbHolder').hide();
-	$('#nuTabHolder').hide();
-	$('.nuActionButton').hide();
-
-	window.onafterprint = function (e) {
-		$(window).off('mousemove', window.onafterprint);
-
-		$('#nuBreadcrumbHolder').show();
-		$('#nuTabHolder').show();
-		$('.nuActionButton').show();
-	};
-
-	window.print();
-
-	setTimeout(function () {
-		$(window).one('mousemove', window.onafterprint);
-	}, 1);
-
-}
-
 function nuClickSearchColumn(e) {
 
 	var c = e.target.id.substr(12);
