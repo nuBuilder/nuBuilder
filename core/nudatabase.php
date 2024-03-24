4d489e4f43c2d7cbc60e1913fe2cdd72d9e8eaab
kev1n <45562597+smalos@users.noreply.github.com>
2024-03-24 09:57:15 +0100

    [Improved] PHP nuDebugResult(): Use nuRunQueryNoDebug()
    DB Version: V.4.5-2024.03.20.0
    Files Version: V.4.5-2024.03.24.1

--- core/nudatabase.php
+++ core/nudatabase.php
@@ -507,7 +507,7 @@ function nuDebugResult($nuDebugMsg, $flag = null){
 		"user_id"	=> nuDebugUserId()
 	];
 
-	nuRunQuery($insert, $params);
+	nuRunQueryNoDebug($insert, $params);
 
 	$nuDebugFlag = $flag ?? '';
 	$proc	= nuProcedure('NUDEBUGRESULTADDED');
